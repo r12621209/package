36400468295258078c30be4eb28f53b9283df353
r12621209 <163800292+r12621209@users.noreply.github.com>
2025-05-06 01:21:46 +0800

    Update sim_5E.R

--- R/sim_5E.R
+++ R/sim_5E.R
@@ -1,4 +1,4 @@
-
+library(doParallel)
 #' Simulates 5 environments with 5-fold and 8-fold cross-validation (CV-O, CV-NO).
 #'
 #' @param omgaG_v0  A diagonal variance-covariance matrix across environments.
