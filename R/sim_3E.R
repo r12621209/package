0b6c2ddee542d673d614804e4b981a1123fc06f5
r12621209 <163800292+r12621209@users.noreply.github.com>
2025-05-08 23:04:50 +0800

    Update sim_3E.R

--- R/sim_3E.R
+++ R/sim_3E.R
@@ -1,4 +1,6 @@
 library(doParallel)
+#' Simulate Cross-Validation in Three Environments
+#' 
 #' Simulates 3 environments with 3-fold and 4-fold cross-validation (CV-O, CV-NO).
 #'
 #' @param omgaG_v0  A diagonal variance-covariance matrix across environments.
